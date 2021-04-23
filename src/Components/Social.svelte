3caba9b7dc37080dc824090816b84b2ae1973de7
Artiom <artiomcav@gmail.com>
2021-04-23 15:28:55 +0300

    Update Social component

---
+++ src/Components/Social.svelte
@@ -0,0 +1,38 @@
+<!--src/Components/Social.svelte-->
+
+<script>
+    export let socialData = {};
+    const { IMAGES_LIST, HEADING } = socialData;
+</script>
+
+<!----------------MARKUP----------------------->
+
+<section id="social-media" class="section grey-bgcolor">
+    <div class="container text-center">
+        <h2 class="title text-center">{HEADING}</h2>
+        <div class="social-icons section-body">
+            {#each IMAGES_LIST as list}
+                <a 
+                    href={list.SOCIAL_LINK}
+                    target="_blank">
+                    <img src={list.IMG_SRC} alt={list.TITLE}>
+                </a>
+            {/each}
+        </div>
+    </div>
+</section>
+
+<!----------------STYLE----------------------->
+
+<style>
+    .social-icons img {
+      width: 75px;
+      transition: 0.5s;
+    }
+    .social-icons a:hover img {
+      transform: translateY(-10px);
+    }
+    a:hover {
+      text-decoration: none;
+    }
+</style>
